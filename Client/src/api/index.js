14f64100942f68a60857ec863a8573e20ca70644
shaikh1994 <shaikh95@live.com>
2023-10-10 23:15:43 +0800

    Update index.js

--- Client/src/api/index.js
+++ Client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://meme-collection.herokuapp.com/posts';
+const url = 'https://memez-server.onrender.com';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
